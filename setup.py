a515a2e8690e425f8976954bd826947656f831dd
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2025-06-18 00:39:32 -0400

    + setup requirement

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         "flask>=2.0.0",
         "gitpython>=3.1.0",
+        "git+https://github.com/inventor2525/assistant_merger.git@master",
     ],
     description="A package for processing AI interaction commands",
     classifiers=[
