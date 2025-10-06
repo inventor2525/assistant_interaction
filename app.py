0c7f1bc3b4301f084377b59502b4426507da46f9
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2025-10-06 11:30:02 -0400

    tabs

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        input_text = request.form.get('command', '').strip()
-        output = process_commands(input_text)
-        return Response(output, mimetype='text/plain')
-    return render_template('index.html')
+	if request.method == 'POST':
+		input_text = request.form.get('command', '').strip()
+		output = process_commands(input_text)
+		return Response(output, mimetype='text/plain')
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=True)
+	app.run(host='0.0.0.0', port=5002, debug=True)
--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="assistant_interaction",
-    version="0.1.0",
-    author="Charlie Mehlenbeck",
-    author_email="charlie_inventor2003@yahoo.com",
-    description="A (currently, likely forever, very shitty) scripting language for facilitating an AI to run bash commands, save and read files, and perform merge request like functions.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/inventor2525/assistant_interaction",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.10.12",
-    install_requires=[
-        "assistant_merger @ git+https://github.com/inventor2525/assistant_merger.git@master",
-    ],
-    extras_require={
-        "dev": ["unittest"],
-    },
+	name="assistant_interaction",
+	version="0.1.0",
+	author="Charlie Mehlenbeck",
+	author_email="charlie_inventor2003@yahoo.com",
+	description="A (currently, likely forever, very shitty) scripting language for facilitating an AI to run bash commands, save and read files, and perform merge request like functions.",
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
+	url="https://github.com/inventor2525/assistant_interaction",
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires=">=3.10.12",
+	install_requires=[
+		"assistant_merger @ git+https://github.com/inventor2525/assistant_merger.git@master",
+	],
+	extras_require={
+		"dev": ["unittest"],
+	},
 )
